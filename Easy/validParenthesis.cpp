0411c241e6b9f7542b757979952381f2cc9baad6
Abdullah <abdullahmuhammad0610@gmail.com>
2026-06-18 01:56:42 +0500

    Worked on the valid parenthesis problem

---
+++ Easy/validParenthesis.cpp
@@ -0,0 +1,54 @@
+#include "iostream"
+#include "stack"
+#include "string"
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+
+        for (char c : s)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if (st.empty())
+                    return false;
+
+                char top = st.top();
+
+                if ((c == ')' && top == '(') ||
+                    (c == ']' && top == '[') ||
+                    (c == '}' && top == '{'))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string s = "({[]})";
+    if (sol.isValid(s))
+    {
+        cout << "string is valid\n";
+    }
+    else
+    {
+        cout << "string is invalid.\n";
+    }
+}
